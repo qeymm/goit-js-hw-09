644e640459cdba0720c4cf93e1d04fe808906099
Kim Enorme <kimenorme15@gmail.com>
2025-05-23 17:40:55 +0800

    update task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let colorChangeInterval = null;
+stopBtn.disabled = true;
+
+function startColorChange() {
+    colorChangeInterval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        stopBtn.disabled = false;
+        startBtn.disabled = true;
+    }, 1000);
+}
+
+function stopColorChange() { 
+    clearInterval(colorChangeInterval);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
+
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
